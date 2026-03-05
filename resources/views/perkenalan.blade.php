7d704741306247117f7688a9bf176f79dfd85e2f
HusainAziz2308 <husainazizalrosyid@gmail.com>
2026-03-05 13:18:24 +0700

    tambah perkenalan

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perkenalan</title>
+</head>
+<body>
+    <h1>Perkenalan</h1>
+    <p>Nama: Husain Aziz Al Rosyid</p>
+    <p>Nim: 4124031</p>
+</body>
+</html>
